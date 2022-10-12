3949baa4c3828120e7c53cfc9c11cfa50246eebe
Satio1230 <m0a211190d@edu,teu.ac.jp>
2022-10-12 18:43:17 +0900

    add weather

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("main.js");
+const CODE = "130010";
+const WEATHERAPI = "https://weather.tsukumijima.net/api/forecast";
+
+fetch(WEATHERAPI + `?city=${CODE}`)
+  .then((res) => res.json())
+    .then(data => {
+      $(".weather_1").html(data["forecasts"][0]["dateLabel"] + ` (${data["forecasts"][0]["date"]})`);
+      $(".weather_data_1").html(data["forecasts"][0]["telop"]);
+      $(".weather_2").html(data["forecasts"][1]["dateLabel"] + ` (${data["forecasts"][1]["date"]})`);
+      $(".weather_data_2").html(data["forecasts"][1]["telop"]);
+    });
