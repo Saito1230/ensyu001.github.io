6e1cb6777d4a4181b7d02e3316ee7d5762d52af5
Satio1230 <m0a211190d@edu,teu.ac.jp>
2022-10-19 18:12:52 +0900

    複数の引数を持つ関数の作成及び、出力のテスト。

--- function_test.js
+++ function_test.js
@@ -1,1 +1,7 @@
-const test = () => alert("TEST!");
+const log_str_1 = (str) => consoleLog(str);
+const log_str_2 = (str_1, str_2 = "Default Str") => {
+  consoleLog(`引数1: {str_1}`);
+  consoleLog(`引数2: {str_2}`);
+}
+
+const consoleLog = (str) => console.log(str);
